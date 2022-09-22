5488cde6bdf313d1bd716611a55a9513c00e06ac
abdullah al harun <abdullahahrn@gmail.com>
2022-09-22 19:17:20 -0400

    update

--- src/main/java/baseUtil/BaseClass.java
+++ src/main/java/baseUtil/BaseClass.java
@@ -1,5 +1,7 @@
 package baseUtil;
-
+/*
+ * Create a maven project of your own and show the URL is running and opening the page. Please add maximize(), deleteAllCookies(), pageLoadTimeOut(), implicitlyWait() in the BaseClass. In the home page, validate the logo, the way I validated. Use click() method to click webeElement, create at least 5. Use @FindBy to find web Elelement. You can use at least one as HOW keyword. Use Thread.sleep() to know use of it. Use enabled and priority keyword in test method along with @Test. Push to the github and share the link below
+ */
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -15,6 +17,8 @@ public class BaseClass {
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\aalha\\eclipse-workspace\\com.wellsfargo\\driver\\chromedriver.exe");
 		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
 		driver.get("https://www.wellsfargo.com/");
 		driver.manage().window().maximize();
 		homePage= new HomePage(driver);			
